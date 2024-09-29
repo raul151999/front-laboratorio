2a7c9ed40baa0a751fd3e7f9bac51f2200f94225
raul andres <rgastanaduil1@upao.edu.pe>
2024-09-29 01:07:04 -0500

    Actualizacion en registro de confirmacion de correo

--- src/app/register-freelancer/register-freelancer.component.ts
+++ src/app/register-freelancer/register-freelancer.component.ts
@@ -23,16 +23,22 @@ export class RegisterFreelancerComponent {
     confirmPassword: ''
   };
 
+  showSuccessModal = false;
+
   constructor(private router: Router) {}
 
   register() {
     if (this.freelancer.password === this.freelancer.confirmPassword) {
       // Lógica para el registro del freelancer
-      console.log('Freelancer registrado:', this.freelancer);
-      this.router.navigate(['/login']); // Redirigir a la página de inicio de sesión después del registro
+      console.log('Desarrollador registrado:', this.freelancer);
+      this.showSuccessModal = true;
     } else {
       alert('Las contraseñas no coinciden.');
     }
+  }
+  closeModal() {
+    this.showSuccessModal = false;
+    this.router.navigate(['/login']);
   }
   openGoogleAuth() {
     const googleAuthURL = 'https://accounts.google.com/v3/signin/identifier?continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ifkv=ARpgrqfahPgL75ggfdYMLu8k27GCVDPwO9gSM48fIpyW_5eFhu9xcvJ0WFDZ_yBKLHv6FyAqHptJsg&rip=1&sacu=1&service=mail&flowName=GlifWebSignIn&flowEntry=ServiceLogin&dsh=S-1743138878%3A1727373242782840&ddm=0';
